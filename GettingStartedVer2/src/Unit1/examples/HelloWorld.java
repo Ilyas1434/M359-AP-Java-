cc9f142b96c6d59a7a3a71fbc9f5db1cdf0e0299
Ilyas1434 <113720884+Ilyas1434@users.noreply.github.com>
2022-09-17 13:56:02 -0500

    Test from home

--- GettingStartedVer2/src/Unit1/examples/HelloWorld.java
+++ GettingStartedVer2/src/Unit1/examples/HelloWorld.java
@@ -3,5 +3,6 @@ package Unit1.examples;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Sameer Ilyas\nRolling Meadows\nGreen\nChess\nWaffles");
+        System.out.println("Test For Github");
     }
 }
