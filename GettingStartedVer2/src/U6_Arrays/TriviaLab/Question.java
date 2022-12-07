bf8620e4e5c2c7bb190d857e44877278686f35ed
Ilyas1434 <113720884+Ilyas1434@users.noreply.github.com>
2022-12-07 14:43:24 -0600

    work at home

---
+++ GettingStartedVer2/src/U6_Arrays/TriviaLab/Question.java
@@ -0,0 +1,107 @@
+package U6_Arrays.TriviaLab;
+
+public class Question {
+    private int questionNum;
+    private String question;
+    private String choiceA;
+    private String choiceB;
+    private String choiceC;
+    private String choiceD;
+    private String answer;
+    private String explanation;
+    private int pointAmount;
+
+    public Question(int questionNum, String question, String choiceA, String choiceB, String choiceC, String choiceD, String answer, String explanation, int pointAmount) {
+        this.questionNum = questionNum;
+        this.question = question;
+        this.choiceA = choiceA;
+        this.choiceB = choiceB;
+        this.choiceC = choiceC;
+        this.choiceD = choiceD;
+        this.answer = answer;
+        this.explanation = explanation;
+        this.pointAmount = pointAmount;
+    }
+
+    public int getQuestionNum() {
+        return questionNum;
+    }
+
+    public void setQuestionNum(int questionNum) {
+        this.questionNum = questionNum;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getChoiceA() {
+        return choiceA;
+    }
+
+    public void setChoiceA(String choiceA) {
+        this.choiceA = choiceA;
+    }
+
+    public String getChoiceB() {
+        return choiceB;
+    }
+
+    public void setChoiceB(String choiceB) {
+        this.choiceB = choiceB;
+    }
+
+    public String getChoiceC() {
+        return choiceC;
+    }
+
+    public void setChoiceC(String choiceC) {
+        this.choiceC = choiceC;
+    }
+
+    public String getChoiceD() {
+        return choiceD;
+    }
+
+    public void setChoiceD(String choiceD) {
+        this.choiceD = choiceD;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public int getPointAmount() {
+        return pointAmount;
+    }
+
+    public void setPointAmount(int pointAmount) {
+        this.pointAmount = pointAmount;
+    }
+
+    /**
+     * Creates a string formatted in the form of a trivia question with four choices, a, b, c, d
+     * @return the entire question plus it's choices and point value right after the question's text
+     */
+    public String toString() {
+        String ret = "";
+        ret += question + " (" + pointAmount + ")" + "\n" + choiceA + "\n" + choiceB + "\n" + choiceC + "\n" + choiceD;
+        return ret;
+    }
+}
---
+++ GettingStartedVer2/src/U6_Arrays/TriviaLab/TriviaGame.java
@@ -0,0 +1,7 @@
+package U6_Arrays.TriviaLab;
+
+public class TriviaGame {
+    Question[] questionArray = new Question[12];
+    int totalPoints = 0;
+    
+}
