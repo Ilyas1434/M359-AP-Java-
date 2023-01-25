f17d87509df1ccfcd3ca7450664205d40400b414
Ilyas1434 <113720884+Ilyas1434@users.noreply.github.com>
2023-01-25 14:45:09 -0600

    Create SelectionSortNotes.java
    asdfasdf

---
+++ GettingStartedVer2/src/Unit7/SelectionSortNotes.java
@@ -0,0 +1,35 @@
+package Unit7;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class SelectionSortNotes {
+    public static void main(String[] args) {
+        ArrayList<Integer> myNums= new ArrayList<Integer>();
+        myNums.add(3);
+        myNums.add(1);
+        myNums.add(0);
+        myNums.add(8);
+        myNums.add(4);
+        myNums.add(1);
+        myNums.add(7);
+        System.out.println(myNums);
+
+        // Selection sort
+        // Outer for loop
+        // goes until 2nd to last value, controls the index where the smallest value swaps into
+
+        for(int i = 0; i < myNums.size() - 1; i++) {
+            int minIndex = i;
+            for(int j = i + 1; i < myNums.size() -1; i++) {
+                if(myNums.get(j) < myNums.get(minIndex)) {
+                    minIndex = j;
+                }
+                int temp = myNums.get(minIndex);
+                myNums.set(minIndex, myNums.get(i));
+                myNums.set(i, temp);
+            }
+        }
+        System.out.println(myNums);
+    }
+}
