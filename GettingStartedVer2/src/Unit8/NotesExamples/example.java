fbcb8bec25b1356ff9490f1d596f400c48f6671b
Ilyas1434 <113720884+Ilyas1434@users.noreply.github.com>
2023-02-24 13:59:37 -0600

    Create example.java

---
+++ GettingStartedVer2/src/Unit8/NotesExamples/example.java
@@ -0,0 +1,15 @@
+package Unit8.NotesExamples;
+
+public class example {
+    public static void main(String[] args) {
+        int[][] nums = new int[3][6];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[0].length; j++) {
+                System.out.print((i + j + 1) + " ");
+            }
+            System.out.println();
+        }
+        double[][] yes = new double[1][3];
+        
+    }
+}
