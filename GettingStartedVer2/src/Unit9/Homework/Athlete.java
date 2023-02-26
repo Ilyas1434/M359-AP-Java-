ab9817fe76d65cc0091c7520ceb29c47abee9db7
Ilyas1434 <113720884+Ilyas1434@users.noreply.github.com>
2023-02-26 12:57:22 -0600

    Update Athlete.java

--- GettingStartedVer2/src/Unit9/Homework/Athlete.java
+++ GettingStartedVer2/src/Unit9/Homework/Athlete.java
@@ -21,11 +21,23 @@ public class Athlete {
     }
     public class TeamAthlete extends Athlete {
         private String teamName;
-        private String teamSport;
-        public TeamAthlete(String name, int age, String teamName, String teamSport) {
+        private String sport;
+        public TeamAthlete(String name, int age, String teamName, String sport) {
             super(name, age);
-            this.teamName teamName;
-
+            this.teamName = teamName;
+            this.sport = sport;
+        }
+        public String getTeamName() {
+            return teamName;
+        }
+        public String getSport() {
+            return sport;
+        }
+        public void setTeamName(String newName) {
+            teamName = newName;
+        }
+        public void setSport(String newSport) {
+            sport = newSport;
         }
     }
 
